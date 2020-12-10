b6509b3ec0c1bb5b68d12bbdcdf3fe4b3c6bcb1f
Dinesh V <dineshbca50@gmail.com>
2020-12-10 11:16:59 +0530

    added one class

--- FizzBuzzproject/src/home/FuzzBuzz.java
+++ FizzBuzzproject/src/home/FuzzBuzz.java
@@ -1,10 +1,30 @@
-package home;
+import java.util.*;
+class FizzBuzz
+{
+	public static void main(String args[])
+	{ 
+		int n = 100;
 
-public class FuzzBuzz {
+		// loop for 100 times
+		for (int i=1; i<=n; i++)								 
+		{
+			//number divisible by 15(divisible by
+			// both 3 & 5), print 'FizzBuzz' in
+			// place of the number
+			if (i%15==0)												 
+				System.out.print("FizzBuzz"+" "); 
+			// number divisible by 5, print 'Buzz' 
+			// in place of the number
+			else if (i%5==0)	 
+				System.out.print("Buzz"+" "); 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			// number divisible by 3, print 'Fizz' 
+			// in place of the number
+			else if (i%3==0)	 
+				System.out.print("Fizz"+" ");
+				
+			else // print the numbers
+				System.out.print(i+" ");						 
+		}
 	}
-
 }
